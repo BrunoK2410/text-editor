d226fd38d0d547e8929f336ac6b2d860f5274f63
BrunoK2410 <brunokatic2410@gmail.com>
2023-09-23 13:10:22 +0200

    Added some global css.

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,48 @@ export default {
   box-sizing: border-box;
 }
 
-html {
-  font-family: "Roboto", sans-serif;
-}
-
 body {
   margin: 0;
+}
+
+a {
+  color: blue;
+}
+
+a:visited {
+  color: #551a8b;
+}
+
+/* CSS beneath this comment is so that dynamically added timer divs in contenteditable div have style applied */
+
+.content {
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+  padding: 10px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+}
+
+.pause {
+  display: flex;
+}
+
+.pause > p {
+  margin: 0 0 0 5px;
+}
+
+.timer-buttons > button {
+  all: unset;
+  cursor: pointer;
+}
+
+.timer-buttons > button:nth-child(1) {
+  margin-right: 12px;
+}
+
+.new-editor:empty:before {
+  content: attr(data-title);
+  color: #f5f7f7;
 }
 </style>
