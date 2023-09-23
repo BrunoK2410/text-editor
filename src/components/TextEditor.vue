d9795e6d4f0b90ada6eefd76de02af6dcef1bee5
BrunoK2410 <brunokatic2410@gmail.com>
2023-09-23 13:14:23 +0200

    Changed logic for Timer in text editor

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -52,7 +52,7 @@
             ></path>
           </svg>
         </button>
-        <button data-title="Insert link">
+        <button data-title="Insert link" @click="openLinkDialog">
           <svg
             focusable="false"
             preserveAspectRatio="xMidYMid meet"
@@ -101,7 +101,7 @@
             ></path>
           </svg>
         </button>
-        <button data-title="Timer" @click="openTimerDialog">
+        <button data-title="Timer" id="timerButton" @click="openTimerDialog">
           <svg
             focusable="false"
             preserveAspectRatio="xMidYMid meet"
@@ -128,87 +128,26 @@
           ref="editor"
           id="editorId"
           @input="isActive"
+          @mouseup="saveSelection"
         ></div>
-        <div class="applied-change" v-if="isTimerSet">
-          <div class="content">
-            <div class="pause">
-              <svg
-                focusable="false"
-                preserveAspectRatio="xMidYMid meet"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                aria-label="Pause response type"
-                aria-hidden="true"
-                width="18"
-                height="18"
-                viewBox="0 0 16 16"
-                role="img"
-                class="bx--btn__icon"
-              >
-                <path
-                  d="M6 4v8H4V4H6M6 3H4C3.4 3 3 3.4 3 4v8c0 .6.4 1 1 1h2c.6 0 1-.4 1-1V4C7 3.4 6.6 3 6 3zM12 4v8h-2V4H12M12 3h-2C9.4 3 9 3.4 9 4v8c0 .6.4 1 1 1h2c.6 0 1-.4 1-1V4C13 3.4 12.6 3 12 3z"
-                ></path>
-              </svg>
-              <p>Pause ({{ receivedDuration }}s)</p>
-            </div>
-            <div class="timer-buttons">
-              <button @click="openTimerDialog">
-                <svg
-                  focusable="false"
-                  preserveAspectRatio="xMidYMid meet"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="#5d8ddf"
-                  aria-label="Edit"
-                  aria-hidden="true"
-                  role="img"
-                  class="bx--btn__icon"
-                >
-                  <path
-                    d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z"
-                  ></path>
-                  <path
-                    d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13"
-                  ></path>
-                </svg>
-              </button>
-              <button @click="deleteTimer">
-                <svg
-                  focusable="false"
-                  preserveAspectRatio="xMidYMid meet"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="#5d8ddf"
-                  aria-label="Delete"
-                  aria-hidden="true"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 32 32"
-                  role="img"
-                  class="bx--btn__icon"
-                >
-                  <path d="M12 12H14V24H12zM18 12H20V24H18z"></path>
-                  <path
-                    d="M4 6V8H6V28a2 2 0 002 2H24a2 2 0 002-2V8h2V6zM8 28V8H24V28zM12 2H20V4H12z"
-                  ></path>
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
     <image-dialog
       :show="showImageDialog"
       @close-dialog="closeImageDialog"
+      :getSelection="storedSelection"
       ref="imageDialog"
     ></image-dialog>
+    <link-dialog
+      :show="showLinkDialog"
+      @close-dialog="closeLinkDialog"
+      :getSelection="storedSelection"
+    ></link-dialog>
     <timer-dialog
       :show="showTimerDialog"
       @close-dialog="closeTimerDialog"
       @get-duration="handleDuration"
+      :insertTimer="insertTimer"
       ref="timerDialog"
     ></timer-dialog>
   </div>
@@ -219,10 +158,12 @@
 <script>
 import ImageDialog from "./ImageDialog.vue";
 import TimerDialog from "./TimerDialog.vue";
+import LinkDialog from "./LinkDialog.vue";
 export default {
   components: {
     ImageDialog,
     TimerDialog,
+    LinkDialog,
   },
   data() {
     return {
@@ -230,15 +171,118 @@ export default {
       isItalic: false,
       showImageDialog: false,
       showTimerDialog: false,
+      showLinkDialog: false,
       isLoading: false,
       isTimerSet: false,
       receivedDuration: 0,
       isMessageSent: false,
+      storedSelection: null,
     };
   },
   methods: {
+    insertTimer() {
+      const timer = `
+
+        <div id="content" class="content" contenteditable="false" v-if="${this.isTimerSet}">
+          <div class="pause">
+            <svg
+              focusable="false"
+              preserveAspectRatio="xMidYMid meet"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              aria-label="Pause response type"
+              aria-hidden="true"
+              width="18"
+              height="18"
+              viewBox="0 0 16 16"
+              role="img"
+              class="bx--btn__icon"
+            >
+              <path
+                d="M6 4v8H4V4H6M6 3H4C3.4 3 3 3.4 3 4v8c0 .6.4 1 1 1h2c.6 0 1-.4 1-1V4C7 3.4 6.6 3 6 3zM12 4v8h-2V4H12M12 3h-2C9.4 3 9 3.4 9 4v8c0 .6.4 1 1 1h2c.6 0 1-.4 1-1V4C13 3.4 12.6 3 12 3z"
+              ></path>
+            </svg>
+            <p>Pause (${this.receivedDuration}s)</p>
+          </div>
+          <div class="timer-buttons">
+            <button id="edit">
+              <svg
+                focusable="false"
+                preserveAspectRatio="xMidYMid meet"
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#5d8ddf"
+                aria-label="Edit"
+                aria-hidden="true"
+                role="img"
+                class="bx--btn__icon"
+              >
+                <path
+                  d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z"
+                ></path>
+                <path
+                  d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13"
+                ></path>
+              </svg>
+            </button>
+            <button id="delete">
+              <svg
+                focusable="false"
+                preserveAspectRatio="xMidYMid meet"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#5d8ddf"
+                aria-label="Delete"
+                aria-hidden="true"
+                width="18"
+                height="18"
+                viewBox="0 0 32 32"
+                role="img"
+                class="bx--btn__icon"
+              >
+                <path d="M12 12H14V24H12zM18 12H20V24H18z"></path>
+                <path
+                  d="M4 6V8H6V28a2 2 0 002 2H24a2 2 0 002-2V8h2V6zM8 28V8H24V28zM12 2H20V4H12z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+        `;
+      const timerAndNewEditor =
+        timer +
+        `<div id="newEditor" class="new-editor" contenteditable="true" data-title="Enter text that will be paused here"></div>`;
+      if (this.isTimerSet === true) {
+        const timerDiv = document.getElementById("content");
+        this.$refs.editor.removeChild(timerDiv);
+        const newEditor = document.getElementById("newEditor");
+        newEditor.insertAdjacentHTML("beforebegin", timer);
+      } else {
+        this.$refs.editor.innerHTML += timerAndNewEditor;
+      }
+      this.isTimerSet = true;
+      setTimeout(() => {
+        const editButton = document.getElementById("edit");
+        const deleteButton = document.getElementById("delete");
+        const newEditor = document.getElementById("newEditor");
+        editButton.addEventListener("click", () => {
+          this.openTimerDialog();
+        });
+        deleteButton.addEventListener("click", () => {
+          this.deleteTimer();
+        });
+        newEditor.focus();
+      });
+    },
+    saveSelection() {
+      const selection = window.getSelection();
+      if (selection.rangeCount > 0) {
+        this.storedSelection = selection.getRangeAt(0).cloneRange();
+      }
+    },
     bold() {
-      // this.isBold = !this.isBold;
       this.$refs.editor.focus();
       if (!this.isBold) {
         document.execCommand("bold");
@@ -291,6 +335,7 @@ export default {
       }, 500);
     },
     openTimerDialog() {
+      this.$refs.editor.focus();
       this.showTimerDialog = true;
       const buttons = document.querySelectorAll(".buttons-wrapper button");
       buttons.forEach((button) => {
@@ -298,6 +343,9 @@ export default {
           button.classList.remove("active");
         }
       });
+    },
+    openLinkDialog() {
+      this.showLinkDialog = true;
     },
     closeImageDialog() {
       this.$refs.imageDialog.resetInput();
@@ -309,34 +357,49 @@ export default {
         this.$refs.timerDialog.resetTimer();
       }
     },
+    closeLinkDialog() {
+      this.showLinkDialog = false;
+    },
     send() {
       const editor = document.getElementById("editorId");
       const output = document.getElementById("output");
       this.isMessageSent = true;
       const message = editor.innerHTML;
       if (this.isTimerSet) {
-        this.deleteTimer();
+        const timerDiv = document.getElementById("content");
+        const newEditor = document.getElementById("newEditor");
+        const delayedMessage = newEditor.innerHTML;
+        timerDiv.remove();
+        newEditor.remove();
+        const instantMessage = editor.innerHTML;
+        output.innerHTML += instantMessage;
         setTimeout(() => {
-          output.innerHTML += message;
+          output.innerHTML += delayedMessage;
         }, this.receivedDuration * 1000);
       } else {
-        output.innerHTML += editor.innerHTML;
+        output.innerHTML += message;
       }
       editor.innerHTML = "";
     },
     handleDuration(data) {
       this.receivedDuration = data;
-      this.isTimerSet = true;
-      const buttons = document.querySelectorAll(".buttons-wrapper button");
-      buttons.forEach((button) => {
-        button.disabled = true;
-      });
+      const timerButton = document.getElementById("timerButton");
+      timerButton.disabled = true;
     },
     deleteTimer() {
-      this.isTimerSet = false;
       if (!this.isMessageSent) {
         this.$refs.timerDialog.resetTimer();
       }
+      this.isTimerSet = false;
+      const editor = this.$refs.editor;
+
+      const timerDiv = document.getElementById("content");
+      const newEditor = document.getElementById("newEditor");
+      const newEditorText = newEditor.innerHTML;
+      timerDiv.remove();
+      newEditor.remove();
+      editor.innerHTML += newEditorText;
+
       const buttons = document.querySelectorAll(".buttons-wrapper button");
       buttons.forEach((button) => {
         button.disabled = false;
@@ -345,6 +408,18 @@ export default {
   },
   mounted() {
     this.isActive();
+    const editButton = document.getElementById("edit");
+    const deleteButton = document.getElementById("delete");
+    if (editButton) {
+      editButton.addEventListener("click", () => {
+        this.openTimerDialog();
+      });
+      if (deleteButton) {
+        deleteButton.addEventListener("click", () => {
+          this.deleteTimer();
+        });
+      }
+    }
   },
 };
 </script>
@@ -426,10 +501,9 @@ export default {
 .loading {
   cursor: wait !important;
 }
-.container {
-  position: relative;
-}
+
 .textarea {
+  position: relative;
   resize: vertical;
   overflow-y: auto;
   width: 700px;
@@ -439,45 +513,5 @@ export default {
   outline-color: #326ecf;
   border: dotted 2px;
   padding: 10px;
-  display: inline-block;
-  vertical-align: middle;
-}
-
-.applied-change {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 98;
-  background-color: #f5f7f7;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.content {
-  display: flex;
-  width: 90%;
-  background-color: white;
-  padding: 10px;
-  justify-content: space-between;
-}
-
-.pause {
-  display: flex;
-}
-
-.pause > p {
-  margin: 0 0 0 5px;
-}
-
-.timer-buttons > button {
-  all: unset;
-  cursor: pointer;
-}
-
-.timer-buttons > button:nth-child(1) {
-  margin-right: 12px;
 }
 </style>
